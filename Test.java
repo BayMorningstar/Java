9579105f0c1b102d589f4077eacfb59f07f8ae32
Halim Ceylan <halim.ceylan7@gmail.com>
2022-03-10 20:58:14 +0300

    ssh deneme

--- Test.java
+++ Test.java
@@ -4,25 +4,25 @@ public class Test {
 
     public static void main(String[] args){
 
-        //DoublyLinkedList<Integer> myList = new DoublyLinkedList<>();
-        //LinkedList<Integer> myList=new LinkedList<>();
-        // CircularLinkedList<Integer> myList=new CircularLinkedList<>();
-        //  myList.addToFront(10);
-        //  myList.addToFront(11);
-        //  myList.addToFront(12);
-        //  myList.addToFront(13);
-        //  myList.selectionSort();
-        //  myList.display();
-          // BinarySearchTree <Integer> myTree = new BinarySearchTree<>();
-          // myTree.insert(8);
-          // myTree.insert(5);
-          // myTree.insert(12);
-          // myTree.insert(10);
-          // myTree.insert(20);
-          // myTree.insert(3);
-          // System.out.println(myTree.search(10));
-          // myTree.inorder();
-          // myTree.searchRecursivly(20);
+        DoublyLinkedList<Integer> myList = new DoublyLinkedList<>();
+        LinkedList<Integer> myList=new LinkedList<>();
+         CircularLinkedList<Integer> myList=new CircularLinkedList<>();
+          myList.addToFront(10);
+          myList.addToFront(11);
+          myList.addToFront(12);
+          myList.addToFront(13);
+          myList.selectionSort();
+          myList.display();
+           BinarySearchTree <Integer> myTree = new BinarySearchTree<>();
+           myTree.insert(8);
+           myTree.insert(5);
+           myTree.insert(12);
+           myTree.insert(10);
+           myTree.insert(20);
+           myTree.insert(3);
+           System.out.println(myTree.search(10));
+           myTree.inorder();
+           myTree.searchRecursivly(20);
 
 
          Graph<Integer> mygGraph = new Graph<Integer>();
@@ -40,33 +40,33 @@ public class Test {
          mygGraph.display();
 
 
-        //        Queue<Integer> q1=new Queue(Integer.class,5);
-//        q1.enqueue(5);
-//        q1.enqueue(15);
-//        q1.enqueue(3);
-//        q1.enqueue(4);
-//        q1.dequeue();
-//        q1.display();
-//        GenericStackOps<Integer> sops=new GenericStackOps<>();
-//        System.out.println(sops.isPalindrome("kapaks"));
+        Queue<Integer> q1=new Queue(Integer.class,5);
+        q1.enqueue(5);
+        q1.enqueue(15);
+        q1.enqueue(3);
+        q1.enqueue(4);
+        q1.dequeue();
+        q1.display();
+        GenericStackOps<Integer> sops=new GenericStackOps<>();
+        System.out.println(sops.isPalindrome("kapaks"));
 
-//        GenericStack<Integer> gs=new GenericStack<>(Integer.class, 5);
-//        //GenericStack<Student> gs2=new GenericStack<>(Student.class, 5);
-//        gs.push(5);
-//        gs.push(6);
-//        gs.push(2);
-//        gs.display();
-//        GenericStackOps<Integer> sops=new GenericStackOps<>();
-//        System.out.println(sops.findMax(gs));
-        //        Stack s=new Stack(5);
-//        s.push(5);
-//        s.push(6);
-//        s.push(2);
-//        //s.pop();
-//
-//        s.display();
-//        System.out.println(StackOperations.search(s,67));
-//        s.display();
+        GenericStack<Integer> gs=new GenericStack<>(Integer.class, 5);
+        GenericStack<Student> gs2=new GenericStack<>(Student.class, 5);
+        gs.push(5);
+        gs.push(6);
+        gs.push(2);
+        gs.display();
+        GenericStackOps<Integer> sops=new GenericStackOps<>();
+        System.out.println(sops.findMax(gs));
+                Stack s=new Stack(5);
+        s.push(5);
+        s.push(6);
+        s.push(2);
+        s.pop();
+
+        s.display();
+        System.out.println(StackOperations.search(s,67));
+        s.display();
 
 
 
